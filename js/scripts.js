7b3130f0a8d1d22b4304e9e069578ed71c366e26
Code Paradox <140871420+WeCodeParadox@users.noreply.github.com>
2024-02-10 10:28:07 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function navcolaps(){
+    document.getElementById("navbarNav").style("display:block")
+}
+
+$(document).ready(function(e) {
+    $('#corosal').load('corosal.html');
+});
+$(document).ready(function(e) {
+    $('#cards').load('cards.html');
+});
